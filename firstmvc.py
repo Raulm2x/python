bddec65f8b0ae42bc0b1da71f5f0a9a320d37ef3
Raul Maestre <73721158+Raulm2x@users.noreply.github.com>
2023-04-13 14:25:46 -0500

    Create firstmvc.py

---
+++ firstmvc.py
@@ -0,0 +1,91 @@
+from tkinter import *
+
+# ---------Modelo----------
+
+class Persona:
+    def __init__(self, estrato):
+        self.estrato = estrato
+
+    def calcular_descuentos(self):
+        descuentos = {}
+        if self.estrato == 1:
+            descuentos["descuento_compra"] = 0.05
+            descuentos["descuento_ropa"] = 0.10
+            descuentos["bono_regalo"] = 10000
+        elif self.estrato == 2:
+            descuentos["descuento_compra"] = 0.03
+            descuentos["descuento_ropa"] = 0.05
+            descuentos["bono_regalo"] = 5000
+        else:
+            descuentos["descuento_compra"] = 0.01
+            descuentos["descuento_ropa"] = 0.03
+            descuentos["bono_regalo"] = 0
+        return descuentos
+
+# ------------Vista-------------
+
+class Interfaz():
+    def __init__(self, master) -> None:
+        self.master = master
+        master.title("Prueba")
+        master.geometry("300x200")
+
+        # Etiqueta de estrato
+        self.labelEstrato = Label(master, text="Estrato:")
+        self.labelEstrato.grid(row=0, column=0, padx=10, sticky='e')
+        
+        #Etiqueta de error en la entrada.
+        self.labelError = Label(master, text="")
+        self.labelError.grid(row=0, column=3, padx=1, sticky='e')
+
+        #Etiqueta de descuentos
+        self.labelDescuentos = Label(master, text="",justify=LEFT)
+        self.labelDescuentos.grid(row=3, column=1, padx=10)
+
+        # Cuadro de entrada de estrato
+        self.entEstrato = Entry(master, width=5)
+        self.entEstrato.grid(row=0, column=1, sticky='w')
+
+        # Crear el boton aceptar
+        self.botonAceptar = Button(
+            master, text="Aceptar", command=self.aceptar)
+        self.botonAceptar.grid(row=2, column=1, pady=5)
+
+    def aceptar(self):
+        controlador.aceptar()
+       
+                
+# ------Controlador-------
+
+class Controller():
+    def __init__(self,interfaz) -> None:
+        self.interfaz = interfaz
+
+    def aceptar(self):
+        self.interfaz.labelDescuentos.config(text="")
+        try:
+            self.estrato = int(self.interfaz.entEstrato.get())
+        except ValueError:
+            self.interfaz.labelError.config(text="Ingrese una opción válida (1-5).", fg='red')
+        else:
+            if self.estrato not in [1,2,3,4,5]:
+                self.interfaz.labelError.config(text="Ingrese una opción válida (1-5).", fg='red')   
+            else:
+                self.interfaz.labelError.config(text="")
+                self.usuario = Persona(self.estrato)
+                self.descuentos = ''
+                
+                for k,v in self.usuario.calcular_descuentos().items():
+                    if v < 1:
+                        self.descuentos += f"{k}: {v*100:.2f}% "+ "\n"
+                    else:
+                        self.descuentos += f"{k}: $ {v}"
+                print(self.descuentos)
+                self.interfaz.labelDescuentos.config(text=self.descuentos)
+        
+
+if __name__ == '__main__':
+    root = Tk()
+    interfaz = Interfaz(root)
+    controlador = Controller(interfaz)
+    root.mainloop()
