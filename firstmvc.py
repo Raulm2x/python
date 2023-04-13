be6a78b755c1d83c66785aef53c959a656b40091
Raul Maestre <73721158+Raulm2x@users.noreply.github.com>
2023-04-13 16:47:04 -0500

    Update firstmvc.py

--- firstmvc.py
+++ firstmvc.py
@@ -1,6 +1,3 @@
-#Programa que calcula los descuentos y bonos con los que cuenta
-#una persona según su estrato económico. 
-
 from tkinter import *
 
 # ---------Modelo----------
@@ -24,6 +21,7 @@ class Persona:
             descuentos["descuento_ropa"] = 0.03
             descuentos["bono_regalo"] = 0
         return descuentos
+    
 
 # ------------Vista-------------
 
@@ -36,13 +34,13 @@ class Interfaz():
         # Etiqueta de estrato
         self.labelEstrato = Label(master, text="Estrato:")
         self.labelEstrato.grid(row=0, column=0, padx=10, sticky='e')
-        
-        #Etiqueta de error en la entrada.
+
+        # Etiqueta de error en la entrada.
         self.labelError = Label(master, text="")
         self.labelError.grid(row=0, column=3, padx=1, sticky='e')
 
-        #Etiqueta de descuentos
-        self.labelDescuentos = Label(master, text="",justify=LEFT)
+        # Etiqueta de descuentos
+        self.labelDescuentos = Label(master, text="", justify=LEFT)
         self.labelDescuentos.grid(row=3, column=1, padx=10)
 
         # Cuadro de entrada de estrato
@@ -55,42 +53,58 @@ class Interfaz():
         self.botonAceptar.grid(row=2, column=1, pady=5)
 
     def aceptar(self):
-        controlador.aceptar()
-       
-                
+        self.labelDescuentos.config(text="")
+        self.estrato = self.entEstrato.get()
+        controlador = Controller(self.estrato)
+
+        #Se validan los datos
+        if not controlador.validar():
+            self.labelError.config(
+                text="Ingrese una opción válida (1-5).", fg='red')
+        else:
+            self.labelError.config(text="")
+            
+            # La vista recibe los datos del modelo desde el controlador
+            self.descuentos = controlador.descuento_usuario()
+            self.descuento = ''
+
+            #Les da formato y los muestra
+            for k, v in self.descuentos.items():
+                if v < 1:
+                    self.descuento += f"{k}: {v*100:.2f}% " + "\n"
+                else:
+                    self.descuento += f"{k}: $ {v}"
+                    
+            self.labelDescuentos.config(text=self.descuento)
+
+
 # ------Controlador-------
 
 class Controller():
-    def __init__(self,interfaz) -> None:
-        self.interfaz = interfaz
+    def __init__(self, estrato) -> None:
+        self.estrato = estrato
 
-    def aceptar(self):
-        #Validación de la entrada
-        self.interfaz.labelDescuentos.config(text="")
+    def validar(self):
         try:
-            self.estrato = int(self.interfaz.entEstrato.get())
+            self.estrato = int(self.estrato)
         except ValueError:
-            self.interfaz.labelError.config(text="Ingrese una opción válida (1-5).", fg='red')
+            return 0
         else:
-            if self.estrato not in [1,2,3,4,5]:
-                self.interfaz.labelError.config(text="Ingrese una opción válida (1-5).", fg='red')
+            if self.estrato not in [1, 2, 3, 4, 5]:
+                return 0
             else:
-                #Creación del objeto usuario a partir de la clase Persona con su No. de estrato
-                self.interfaz.labelError.config(text="")
-                self.usuario = Persona(self.estrato)
-                self.descuentos = ''
-                
-                #Presentación de los datos del diccionario 'descuentos'
-                for k,v in self.usuario.calcular_descuentos().items():
-                    if v < 1:
-                        self.descuentos += f"{k}: {v*100:.2f}% "+ "\n"
-                    else:
-                        self.descuentos += f"{k}: $ {v}"
-                self.interfaz.labelDescuentos.config(text=self.descuentos)
-        
-#Inicio de la aplicación
+                return 1
+
+    def crear_usuario(self):
+        self.usuario = Persona(self.estrato)
+
+    def descuento_usuario(self):
+        self.crear_usuario()
+        return self.usuario.calcular_descuentos()
+
+
 if __name__ == '__main__':
     root = Tk()
     interfaz = Interfaz(root)
-    controlador = Controller(interfaz)
     root.mainloop()
+
